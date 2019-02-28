3f00302539b096be5e38b463408dffca7cdb89b5
mana-murakami <mmurakami@nvidia.com>
2019-02-28 14:30:33 +0900

    update horovodo install script

--- examples/keras_mnist/run_nvprof.sh
+++ examples/keras_mnist/run_nvprof.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#$ -l rt_F=4 
 #$ -l h_rt=1:00:00 
 #$ -N keras_mnist_test 
 #$ -cwd
@@ -7,4 +8,4 @@ source /etc/profile.d/modules.sh
 source $PWD/load_modules.sh
 source $PWD/horovod/bin/activate
           
-mpiexec -n 16 -map-by ppr:4:node nvprof -f -o $PWD/examples/keras_mnist/keras_mnist.%q{OMPI_COMM_WORLD_RANK}.nvprof python3 $PWD/examples/keras_mnist/mnist.py
+mpiexec -n 16 -map-by ppr:4:node nvprof -f -o $PWD/examples/keras_mnist/keras_mnist.%q{OMPI_COMM_WORLD_RANK}.nvvp python3 $PWD/examples/keras_mnist/mnist.py
--- examples/keras_mnist/run_nvtx.sh
+++ examples/keras_mnist/run_nvtx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#$ -l rt_F=4 
 #$ -l h_rt=1:00:00 
 #$ -N keras_mnist_test 
 #$ -cwd
@@ -10,4 +11,4 @@ source $PWD/load_modules.sh
 source $PWD/horovod_cupy/bin/activate
           
 #mpiexec --output-filename $PWD/keras_mnist -n 16 -map-by ppr:4:node python3 $PWD/mnist.py
-mpiexec -n 16 -map-by ppr:4:node nvprof -f -o $PWD/examples/keras_mnist/keras_mnist.%q{OMPI_COMM_WORLD_RANK}.nvprof python3 $PWD/examples/keras_mnist/mnist_nvtx.py
+mpiexec -n 16 -map-by ppr:4:node nvprof -f -o $PWD/examples/keras_mnist/keras_mnist.%q{OMPI_COMM_WORLD_RANK}.nvvp python3 $PWD/examples/keras_mnist/mnist_nvtx.py
