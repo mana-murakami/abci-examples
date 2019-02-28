781eb8a700fe4215eab4ee51c80608f4cd045ee1
mana-murakami <mmurakami@nvidia.com>
2019-02-28 13:42:50 +0900

    added run script for nvtx profile exmaple

---
+++ examples/keras_mnist/run_nvtx.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#$ -l h_rt=1:00:00 
+#$ -N keras_mnist_test 
+#$ -cwd
+
+
+
+source /etc/profile.d/modules.sh
+source $PWD/load_modules.sh
+source $PWD/horovod_cupy/bin/activate
+          
+#mpiexec --output-filename $PWD/keras_mnist -n 16 -map-by ppr:4:node python3 $PWD/mnist.py
+mpiexec -n 16 -map-by ppr:4:node nvprof -f -o $PWD/examples/keras_mnist/keras_mnist.%q{OMPI_COMM_WORLD_RANK}.nvprof python3 $PWD/examples/keras_mnist/mnist_nvtx.py
